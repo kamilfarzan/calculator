9fc71aebf1b4f8a8af9def2a6406ce525e60f1e9
Kamil Farzan <kmlfrzn27@gmail.com>
2023-08-09 23:37:08 +0530

    created layout, basic structure, added functionality to append numbers

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 import { add, subtract, multiply, divide } from "./js/functions.js";
 
+let displayValue = 0;
+
+function setEventListeners() {
+  const numbers = document.querySelectorAll(`#digit`);
+  numbers.forEach((el) => {
+    el.addEventListener(`click`, (e) => {
+      e.preventDefault();
+      appendDisplayValue(el.value);
+    });
+  });
+
+  const clear = document.querySelector(`#clear`);
+  clear.addEventListener(`click`, clearDisplayValue);
+}
+setEventListeners();
+
+function clearDisplayValue() {
+  const screen = document.querySelector(`#screen`);
+  screen.textContent = `0`;
+  displayValue = `0`;
+}
+
+function appendDisplayValue(val) {
+  const screen = document.querySelector(`#screen`);
+  if (screen.textContent == `0`) {
+    screen.textContent = `${val.toString()}`;
+  } else {
+    screen.textContent = `${screen.textContent + val.toString()}`;
+  }
+  displayValue = screen.textContent;
+}
+
 function operate(a, operator, b) {
   switch (operator) {
     case "+":
@@ -16,5 +48,3 @@ function operate(a, operator, b) {
 let firstNum = 2;
 let secondNum = 2;
 let operator = "+";
-
-console.log(operate(firstNum, operator, secondNum));
